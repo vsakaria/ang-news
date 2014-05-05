f4eecde5ecba2d1668ab968566490e1148fe4391
Vishal Sakaria <vsakaria@gmail.com>
2014-05-05 11:35:07 +0100

    3 way data binding with server

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,20 +1,23 @@
 'use strict';
 /* global app:true */
 
-var app = angular.module('angNewsApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-]);
+var app = angular
+    .module('angNewsApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'firebase'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/posts.html',
+                controller: 'PostsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
 
-app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-        controller: 'PostsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.constant('FIREBASE_URL', 'https://scorching-fire-4068.firebaseio.com/');
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,19 +1,16 @@
 'use strict';
 
 app.controller('PostsCtrl', function ($scope, Post) {
-    $scope.posts = Post.get();
-    $scope.post = {url: 'http://', title: ''};
+    $scope.posts = Post.all;
+    $scope.post = {url: 'http://', 'title': ''};
 
     $scope.submitPost = function () {
-      Post.save($scope.post, function (ref) {
-        $scope.posts[ref.name] = $scope.post;
-        $scope.post = {url: 'http://', title: ''};
+      Post.create($scope.post).then(function () {
+        $scope.post = {url: 'http://', 'title': ''};
       });
     };
 
     $scope.deletePost = function (postId) {
-      Post.delete({id: postId}, function () {
-        delete $scope.posts[postId];
-      });
+      Post.delete(postId);
     };
   });
