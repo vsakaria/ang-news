aa60cd4bb9ccfff667696d70ab2d2b828bafab5d
Vishal Sakaria <vsakaria@gmail.com>
2014-05-06 12:22:47 +0100

    Dist removed

--- Gruntfile.js
+++ Gruntfile.js
@@ -281,6 +281,19 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        },
+        {
+          expand: true,
+          dest: '<%= yeoman.dist %>',
+          cwd: 'heroku',
+          src: '*',
+          rename: function (dest, src) {
+              var path = require('path');
+              if (src === 'distpackage.json') {
+                return path.join(dest, 'package.json');
+              }
+              return path.join(dest, src);
+            }
         }]
       },
       styles: {
