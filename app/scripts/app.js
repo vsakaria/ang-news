0a8460e6626dacfdb382274f091eb3943435ff60
Vishal Sakaria <vsakaria@gmail.com>
2014-05-13 16:41:47 +0100

    Animations added on boxes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,7 @@ var app = angular
         'ngResource',
         'ngSanitize',
         'ngRoute',
+        'ngAnimate',
         'firebase'
     ])
     .config(['$routeProvider', function($routeProvider) {
@@ -25,3 +26,58 @@ var app = angular
     }])
 
 .constant('FIREBASE_URL', 'https://scorching-fire-4068.firebaseio.com/');
+
+app.directive(['post', function(post) {
+  var controller = function($scope) {
+    $scope.showAnswer = false;
+  };
+
+  return {
+    restrict: 'C',
+    scope: false,
+    controller: controller
+  };
+}]);
+
+app.animation('.answer-animation', function(){
+
+  CSSPlugin.defaultTransformPerspective = 1000;
+
+  TweenMax.set($("div.back"), {rotationX:-180});
+
+  $.each($("div.box"), function(i,element)
+  {
+    console.log(element);
+    var frontCard = $(this).children("div.front")
+    var backCard = $(this).children("div.back")
+    var tl = new TimelineMax({paused:true})
+
+    tl
+      .to(frontCard, 1, {rotationX:180})
+      .to(backCard, 1, {rotationX:0},0)
+
+    this.animation = tl;
+  });
+
+  return {
+    beforeAddClass: function(element, className, done){
+      if (className == 'answer') {
+        var el = element.find('.box')[0];
+        el.animation.play();
+      }
+      else {
+        done();
+      }
+    },
+
+    beforeRemoveClass: function(element, className, done) {
+      if (className == 'answer') {
+        var el = element.find('.box')[0];
+        el.animation.reverse();
+      }
+      else {
+        done();
+      }
+    }
+  };
+});
